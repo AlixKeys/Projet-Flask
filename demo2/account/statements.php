642b4b70ab542e131671f7efad30710615cccf9b
crepinowg <toviawoukplolacrepin@gmail.com>
2025-05-17 09:34:58 +0000

    Ajout du template metronic pour l'intégration au projet de détection de spam

---
+++ demo2/account/statements.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<!--
+Author: Keenthemes
+Product Name: Metronic - Bootstrap 5 HTML, VueJS, React, Angular & Laravel Admin Dashboard Theme 
+Purchase: https://1.envato.market/EA4JP
+Website: http://www.keenthemes.com
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
+-->
+<html lang="en">
+	<!--begin::Head-->
+	
+<!-- Mirrored from preview.keenthemes.com/metronic8/demo2/account/billing.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 11 Jun 2022 18:16:46 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+		<title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular &amp; Laravel by Keenthemes</title>
+		<meta charset="utf-8" />
+		<meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
+		<meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="article" />
+		<meta property="og:title" content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular &amp; Laravel Admin Dashboard Theme" />
+		<meta property="og:url" content="https://keenthemes.com/metronic" />
+		<meta property="og:site_name" content="Keenthemes | Metronic" />
+		<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+		<link rel="shortcut icon" href="../assets/media/logos/favicon.ico" />
+		<!--begin::Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<!--end::Fonts-->
+		<!--begin::Page Vendor Stylesheets(used by this page)-->
+		<link href="../assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Page Vendor Stylesheets-->
+		<!--begin::Global Stylesheets Bundle(used by all pages)-->
+		<link href="../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Global Stylesheets Bundle-->
+		<!--Begin::Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src= '../../../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5FS8GGP');</script>
+		<!--End::Google Tag Manager -->
+	</head>
+	<!--end::Head-->
+	<!--begin::Body-->
+	<body id="kt_body" style="background-image: url(../assets/media/patterns/header-bg.jpg)" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled">
+		<!--Begin::Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+		</noscript>
+		<!--End::Google Tag Manager (noscript) -->
+		<!--begin::Main-->
+		<!--begin::Root-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Page-->
+			<div class="page d-flex flex-row flex-column-fluid">
+				<!--begin::Wrapper-->
+				<div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+					<?php require_once('../account/first.html')?>
+					<!--begin::Container-->
+					<div id="kt_content_container" class="d-flex flex-column-fluid align-items-start container-xxl">
+						<!--begin::Post-->
+						<div class="content flex-row-fluid" id="kt_content">
+							<!--begin::Navbar-->
+							<div class="card mb-5 mb-xl-10">
+								<div class="card-body pt-9 pb-0">
+									<?php require_once('../account/overviewInclude.html')?>
+									<!--begin::Navs-->
+									<!--begin::Navs-->
+									<ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder">
+										<!--begin::Nav item-->
+										<li class="nav-item mt-2">
+											<a class="nav-link text-active-primary ms-0 me-10 py-5 " href="overview.php">Aperçu</a>
+										</li>
+										<!--end::Nav item-->
+										<!--begin::Nav item-->
+										<li class="nav-item mt-2">
+											<a class="nav-link text-active-primary ms-0 me-10 py-5 " href="settings.php">Paramètres</a>
+										</li>
+										<!--end::Nav item-->
+										
+										<!--begin::Nav item-->
+										<li class="nav-item mt-2">
+											<a class="nav-link text-active-primary ms-0 me-10 py-5 " href="billing.php">Like</a>
+										</li>
+										<!--end::Nav item-->
+										<!--begin::Nav item-->
+										<li class="nav-item mt-2">
+											<a class="nav-link text-active-primary ms-0 me-10 py-5 active" href="statements.php">Publication</a>
+										</li>
+										<!--end::Nav item-->
+										
+									</ul>
+									<!--begin::Navs-->
+								</div>
+							</div>
+							<!--end::Navbar-->
+							<!--begin::Col-->
+								<div class="col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10">
+										<!--begin::Header-->
+										<div class="card-header pt-5">
+											<!--begin::Title-->
+											<div class="card-title d-flex flex-column">
+												<!--begin::Amount-->
+									
+									<!--end::Card widget 16-->
+									<!--begin::Card widget 7-->
+									<div class="col-xl-6">
+											<!--begin::Mixed Widget 3-->
+											<div class="card h-100 bgi-no-repeat bgi-size-cover card-xl-stretch mb-5 mb-xl-8" style="background-image:url('assets/media/misc/bg-2.jpg')">
+												<!--begin::Body-->
+												<div class="card-body d-flex flex-column justify-content-between">
+													<!--begin::Title-->
+													<div class="text-white fw-bolder fs-2">
+													<h2 class="fw-bolder text-white mb-2">Dernières publications</h2></div>
+													<!--end::Title-->
+													<!--begin::Link-->
+													<a href='#' class="text-warning fw-bold" data-bs-toggle="modal" data-bs-target="#kt_modal_create_campaign">
+													
+													<!--end::Svg Icon--></a>
+													<!--end::Link-->
+												</div>
+												<!--end::Body-->
+												<!--begin::Card body-->
+												<div class="card-body d-flex flex-column justify-content-end pe-0">
+													
+													<!--begin::Users group-->
+													<div class="symbol-group symbol-hover flex-nowrap"  style="margin-bottom: -45px;">
+														<div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" title="Alan Warden">
+															<span class="symbol-label bg-warning text-inverse-warning fw-bolder">A</span>
+														</div>
+														<div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" title="Michael Eberon">
+															<img alt="Pic" src="../assets/media/avatars/300-11.jpg" />
+														</div>
+														<div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" title="Susan Redwood">
+															<span class="symbol-label bg-primary text-inverse-primary fw-bolder">S</span>
+														</div>
+														<div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" title="Melody Macy">
+															<img alt="Pic" src="../assets/media/avatars/300-2.jpg" />
+														</div>
+														<div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" title="Perry Matthew">
+															<span class="symbol-label bg-danger text-inverse-danger fw-bolder">P</span>
+														</div>
+														<div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" title="Barry Walter">
+															<img alt="Pic" src="../assets/media/avatars/300-12.jpg" />
+														</div>
+														<a href="#" class="symbol symbol-35px symbol-circle" data-bs-toggle="modal" data-bs-target="#kt_modal_view_users">
+															<span class="symbol-label bg-dark text-gray-300 fs-8 fw-bolder">+42</span>
+														</a>
+													</div>
+													<!--end::Users group-->
+												</div>
+												<!--end::Card body-->
+											</div>
+											<!--end::Mixed Widget 3-->
+										</div>
+										<!--end::Col-->
+								</div>
+								<!--end::Col-->
+		<!--end::Modal - Invite Friend-->
+		<!--end::Modals-->
+		<!--begin::Javascript-->
+		<script>var hostUrl = "../assets/index.html";</script>
+		<!--begin::Global Javascript Bundle(used by all pages)-->
+		<script src="../assets/plugins/global/plugins.bundle.js"></script>
+		<script src="../assets/js/scripts.bundle.js"></script>
+		<!--end::Global Javascript Bundle-->
+		<!--begin::Page Vendors Javascript(used by this page)-->
+		<script src="../assets/plugins/custom/datatables/datatables.bundle.js"></script>
+		<!--end::Page Vendors Javascript-->
+		<!--begin::Page Custom Javascript(used by this page)-->
+		<script src="../assets/js/widgets.bundle.js"></script>
+		<script src="../assets/js/custom/widgets.js"></script>
+		<script src="../assets/js/custom/apps/chat/chat.js"></script>
+		<script src="../assets/js/custom/intro.js"></script>
+		<script src="../assets/js/custom/utilities/modals/upgrade-plan.js"></script>
+		<script src="../assets/js/custom/utilities/modals/create-app.js"></script>
+		<script src="../assets/js/custom/utilities/modals/offer-a-deal/type.js"></script>
+		<script src="../assets/js/custom/utilities/modals/offer-a-deal/details.js"></script>
+		<script src="../assets/js/custom/utilities/modals/offer-a-deal/finance.js"></script>
+		<script src="../assets/js/custom/utilities/modals/offer-a-deal/complete.js"></script>
+		<script src="../assets/js/custom/utilities/modals/offer-a-deal/main.js"></script>
+		<script src="../assets/js/custom/utilities/modals/new-card.js"></script>
+		<script src="../assets/js/custom/utilities/modals/new-address.js"></script>
+		<script src="../assets/js/custom/utilities/modals/users-search.js"></script>
+		<!--end::Page Custom Javascript-->
+		<!--end::Javascript-->
+	</body>
+	<!--end::Body-->
+
+<!-- Mirrored from preview.keenthemes.com/metronic8/demo2/account/billing.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 11 Jun 2022 18:16:47 GMT -->
+</html>
