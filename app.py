ed8dfcf8c4b7c861191bfe8416c79ca9e8ff056e
Alix <keysabova94@gmail.com>
2025-05-19 16:58:08 +0000

    commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-import joblib
 
-# Chargement du modèle sauvegardé
-model = joblib.load('spam_model.pkl')
+app = Flask(__name__)
 
-# Exemple d'utilisation
-prediction = model.predict(["Hello, you've won a prize!"])
-print(prediction)  # Affichera 1 (spam) ou 0 (non-spam)
+@app.route('/home')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
