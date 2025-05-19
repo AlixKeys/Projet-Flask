ec2fbba4703549bfb43f67432362af7e64d3708c
Alix <keysabova94@gmail.com>
2025-05-19 18:07:35 +0000

    commit

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import joblib
 
 app = Flask(__name__)
+model = joblib.load('spam_model.pkl')
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('compose.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['message']  # récupère le message depuis le formulaire
+    prediction = model.predict([message])[0]  # 0 ou 1
+    result = "SPAM" if prediction == 1 else "NON-SPAM"
+    return render_template('compose.html', prediction=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
